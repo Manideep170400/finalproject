bc9428d13e9e50a17ef46a30d7dc36db4a0f008a
Manideep170400 <116505823+Manideep170400@users.noreply.github.com>
2024-08-09 13:42:19 +0530

    changes

---
+++ src/DASHBOARD/createTIM.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "../App.css";
+import { Link } from "react-router-dom";
+import UserProfile from "../finalPage/mainPage";
+
+function CreateTim() {
+  return (
+    <div className="header__wrapper">
+      <div>
+        <p>Dashboard</p>
+        <Link to="/userProfile">UserProfile</Link>
+      </div>
+      <div>
+        <p>Dashboard</p>
+      </div>
+    </div>
+  );
+}
+
+export default CreateTim;
---
+++ src/finalPage/mainPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function UserProfile() {
+  return (
+    <div>
+      <p>UserProfile</p>
+    </div>
+  );
+}
+
+export default UserProfile;
