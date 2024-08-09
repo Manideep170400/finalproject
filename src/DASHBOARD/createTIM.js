e228459195cc178685474ada7abc20032aa5e953
Manideep170400 <116505823+Manideep170400@users.noreply.github.com>
2024-08-09 14:17:42 +0530

    changes

--- src/DASHBOARD/createTIM.js
+++ src/DASHBOARD/createTIM.js
@@ -7,7 +7,13 @@ function CreateTim() {
   return (
     <div className="header__wrapper">
       <div>
-        <p>Dashboard</p>
+        <p
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+        >
+          CreateTim
+        </p>
         <Link to="/userProfile">UserProfile</Link>
       </div>
       <div>
